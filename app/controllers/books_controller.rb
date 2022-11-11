344fb6bf15eb6eacd3c1e45bfd046db0d41ec90e
EC2 Default User <ec2-user@ip-172-31-22-26.ap-northeast-1.compute.internal>
2022-11-11 05:05:35 +0000

    [Update]課題２　フラッシュメッセージのバグ取り中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,24 +9,37 @@ class BooksController < ApplicationController
   # どうやら、form_withヘルパーを使うと、createアクションに自動で値を渡してくれるみたい
   def create
     # データを受け取るための新規登録用のインスタンスを作成
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     # データをdbに保存するためのsaveメソッドを実行
-    book.save
-    # showページにリダイレクト(いったんindexページにリダイレクトする)
-    # redirect_to '/books'
-    # showページにリダイレクト
-    redirect_to book_path(book.id)
+    if @book.save
+      # フラッシュメッセージを定義
+      flash[:notice] = "Book was successfully created."
+      # showページにリダイレクト
+      redirect_to book_path(@book.id)
+    else
+      # indexページにリダイレクト（ページを移動せずそのまま更新）
+      # render :indexするとindexアクションをすっ飛ばすため、ここにBook.allのインスタンスを作る必要あり
+      @books = Book.all
+      # redirect_to books_path
+      render :index
+    end
   end
 
   def edit
     @book = Book.find(params[:id])
+      flash[:notice] = "Book was successfully updated."
   end
 
   # 編集のアクションを作成
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    # updateが成功・失敗した時を記述
+    if book.update(book_params)
+      redirect_to book_path(book.id) #showページにリダイレクト
+    else
+      @book = book
+      render :edit #editページにそのまま飛ぶ
+    end
   end
 
   def show
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+
+  # バリデーションの記述
+  validates :title,presence: true
+  validates :body,presence: true
 end
